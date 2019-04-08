bfcad8b7d4e2ff0d643c511cf340f63926506b03
apomic80 <michele.aponte@gmail.com>
2019-04-08 10:24:58 +0200

    added bootstrap and grid component

--- Silverlight2Angular.Angular/src/app/controls/grid/grid.component.ts
+++ Silverlight2Angular.Angular/src/app/controls/grid/grid.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { BasecontrolComponent } from '../basecontrol/basecontrol.component';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
-export class GridComponent implements OnInit {
+export class GridComponent extends BasecontrolComponent implements OnInit {
 
-  constructor() { }
+  public rowDefinitions = [];
+  public columnDefinitions = [];
+  public controls = [];
+
+  constructor() { super(); }
 
   ngOnInit() {
+    this.loadRowDefinitions();
+    this.loadColDefinitions();
+    this.loadControls();
+  }
+
+  public getControl(row: number, column: number) {
+    const rowValue = row.toString();
+    const columnValue = column.toString();
+    return this.controls.find(c => c['@Grid.Row'] === rowValue && c['@Grid.Column'] === columnValue);
+  }
+
+  public loadRowDefinitions() {
+    const rows = this.schema['Grid.RowDefinitions'] ? this.schema['Grid.RowDefinitions'].RowDefinition : null;
+    if (rows) {
+      if (Array.isArray(rows)) {
+        rows.forEach(row => {
+          this.rowDefinitions.push(row);
+        });
+      } else {
+        this.rowDefinitions.push(rows);
+      }
+    }
+  }
+
+  public loadColDefinitions() {
+    const cols = this.schema['Grid.ColumnDefinitions'] ? this.schema['Grid.ColumnDefinitions'].ColumnDefinition : null;
+    if (cols) {
+      if (Array.isArray(cols)) {
+        cols.forEach(col => {
+          this.columnDefinitions.push(col);
+        });
+      } else {
+        this.columnDefinitions.push(cols);
+      }
+    }
+  }
+
+  loadControls() {
+    for (const key in this.schema) {
+      if (this.schema.hasOwnProperty(key) && !key.startsWith('@')
+        && key.indexOf('.') === -1 && key !== 'controlName') {
+        const element = this.schema[key];
+
+        if (Array.isArray(element)) {
+          element.forEach(arrayElement => {
+            arrayElement.controlName = key;
+            this.controls.push(arrayElement);
+          });
+        } else {
+          element.controlName = key;
+          this.controls.push(element);
+        }
+      }
+    }
   }
 
 }
